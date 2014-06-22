f9ea4ec8dc466ae4a11eb5d1d8a10feca8b7add3
denezra <den.segunial@gmail.com>
2014-06-22 23:47:40 +0800

    Add and Modify

--- Tutorials/boilerplate/api/index.js
+++ Tutorials/boilerplate/api/index.js
@@ -1,19 +1,22 @@
 //Core Modules
-var fs 		  	 = require('fs'),
-	 http 	  	 = require('http'),
-	 path 	  	 = require('path'),
-	 url			 = require('url'),
-	 queryString = require('querystring');
+var fs 		  	  = require('fs'),
+	 http 	  	  = require('http'),
+	 path 	  	  = require('path'),
+	 url		  = require('url'),
+	 queryString  = require('querystring'),
+	 EventEmitter = require('events').EventEmitter;
+
 var pathArray	= {
-	path			: './content/',
+	path		: './content/',
 	extension	: '.js'
 }
 //Test Modules
-var arrayValidator 	= require('./test_module/arrayValidator.js'),
+var arrayValidator 		= require('./test_module/arrayValidator.js'),
 	 urlRequestParse	= require('./test_module/urlRequestParse.js');
 
-var arrayValid = new arrayValidator(),
-	 urlParser  = new urlRequestParse();
+var  arrayValid 	= new arrayValidator(),
+	 urlParser  	= new urlRequestParse(),
+	 eventTrigger	= new EventEmitter();;
 	 
 http.createServer(function (request, response) {
 	//New array for the path
@@ -23,11 +26,61 @@ http.createServer(function (request, response) {
 	//Looping for substring array of pathFile
 		arrayValid.begin(pathFile, pathFileArray);
 
-	console.log('pathFileArray ',pathFileArray);
+		urlParser.requestParser(request, response, url, queryString, eventTrigger)
 
+	eventTrigger.on('data-collected-end', function (response, query){
+		var queryDataString = query,
+			fileLength 		= pathFileArray.length,
+			fileParent = (pathFileArray.length != 0 ? pathFileArray[fileLength-fileLength] + '/' : '');
 
-	//response.writeHead(200); 
-	//response.end('Page Not Found');
+		if(queryDataString == '') {
+			//No query string
+			jsFile 		= 'index';
+			jsFunction 	= 'load';
+		} else {
+			//Has query string
+			jsFile 		= (fileLength <= 1 ? 'index'  : pathFileArray[fileLength-1]);
+			jsFunction 	= (pathFileArray.length <= 1 ? 'load' : pathFileArray[fileLength - 1]);
+		}
+		//Concatenate the js Files with path and extension
+		var files = pathArray.path + fileParent + jsFile + pathArray.extension;
+		//Use fs exist event
+		fs.exists(files, function (exists) {
+			//if file exit
+			if (exists) {
+				//fs read the file
+				fs.readFile(files, function (err, data) {
+					//Error Handler
+					if(err) { 
+						response.writeHead(500); 
+						response.end('ServerError!'); 
+						return; 
+					};
+					//Access the js File by using require.
+					var modulizeFiles = require(files)(),
+					//Variable counter
+						 counter 		= 0;
+					//Check for all the properties inside of an object;
+					for(counter in modulizeFiles) {
+						//If object has own property
+						if(modulizeFiles.hasOwnProperty(counter)){
+							response.writeHead(200, {'Content-Type': 'text/html'});
+							response.write(modulizeFiles[jsFunction](queryDataString));
+							response.end();	
+						} else {
+							response.writeHead(404); 
+							response.end('Method Not Found');
+						}
+					}
+				});
+				//File Exist return
+				return;
+			}
+			//File doesn't exist
+			response.writeHead(404); 
+			response.end('Page Not Found');
+		});
+	});
 })
 //Listen to port
 .listen(8080);
--- Tutorials/boilerplate/api/test_module/urlRequestParse.js
+++ Tutorials/boilerplate/api/test_module/urlRequestParse.js
@@ -6,66 +6,41 @@ module.exports = (function() {
 	-------------------------------*/
 	/* Private Properties
 	-------------------------------*/
+	var _queryDataString = '',
+		_bodyData		 = '';
 	/* Loader
 	-------------------------------*/
 	/* Construct
 	-------------------------------*/
 	/* Public Methods
 	-------------------------------*/
-	public.requestParser = 
+	public.requestParser = function(request, response, url, queryString, callback) {
+		switch(request.method) {
+			case 'GET'    :
+				_queryDataString = url.parse(decodeURI(request.url), true).query;
+				break;
+			//POST and PUT method
+			case 'PUT'	  :
+			case 'POST'   :
+				request.on('data', function (data) { 
+				_bodyData += data; 
+				});
+				request.on('end', function() {
+				_queryDataString =  queryString.parse(_bodyData);
+				});
+			break;
+			//DELETE method???	
+			case 'DELETE' :
+				_queryDataString = 'empty';
+				break;
+		}
+		setTimeout(function() {
+			callback.emit('data-collected-end', response, _queryDataString);
+		},1)
+	};
 	/* Private Methods
 	-------------------------------*/  
 	/* Adaptor
 	-------------------------------*/
 	return c;
 })(); 
-
-// //Class Function
-// var requestParser = (function() {
-// 	var c = function(){}, public = c.prototype;
-// 	/* Constant
-// 	--------------------------------*/
-// 	/* Public Properties
-// 	-------------------------------*/
-// 	/* Private Properties
-// 	-------------------------------*/
-// 	/* Loader
-// 	-------------------------------*/
-// 	/* Construct
-// 	-------------------------------*/
-// 	/* Public Methods
-// 	-------------------------------*/
-// 	//Get the request method and response
-// 	public.requestMethod = function(request, response) {
-// 		var queryDataString = '',
-// 			 bodyData 		  = '';
-// 		switch(request.method) {
-// 			//GET method
-// 			case 'GET'    :
-// 				queryDataString = url.parse(decodeURI(request.url), true).query;
-// 				break;
-// 			//POST and PUT method
-// 			case 'PUT'	  :
-// 			case 'POST'   :
-// 				request.on('data', function (data) { 
-// 					bodyData += data; 
-// 				});
-// 				request.on('end', function() {
-// 					queryDataString =  queryString.parse(bodyData);
-// 				});
-// 				break;
-// 			//DELETE method???	
-// 			case 'DELETE' :
-// 				queryDataString = 'empty';
-// 				break;
-// 		};
-// 		setTimeout(function() {
-// 			dataEvent.emit('data-collected-end', response, queryDataString);
-// 		},1)
-// 	};
-// 	/* Private Methods
-// 	-------------------------------*/
-// 	/* Adaptor
-// 	-------------------------------*/
-// 	return c;
-// })();
